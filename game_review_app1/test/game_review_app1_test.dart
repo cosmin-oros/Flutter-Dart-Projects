307eb2cecab32c120cb7a3c061f161b1f3c1f08a
cosmin-oros <oroscosmin09@gmail.com>
2022-11-16 11:45:55 +0200

    update

--- game_review_app1/test/game_review_app1_test.dart
+++ game_review_app1/test/game_review_app1_test.dart
@@ -1,4 +1,5 @@
 import 'package:game_review_app1/game_review_app1.dart';
+/*
 import 'package:test/test.dart';
 
 void main() {
@@ -6,3 +7,4 @@ void main() {
     expect(calculate(), 42);
   });
 }
+*/
